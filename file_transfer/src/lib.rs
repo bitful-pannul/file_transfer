609ad0b340280c020d01904de12348114a6f362d
bitful-pannul <bitful-pancreas@protonmail.com>
2024-01-10 16:23:47 -0300

    clean start

--- file_transfer/src/lib.rs
+++ file_transfer/src/lib.rs
@@ -1,7 +1,7 @@
+use serde::{Deserialize, Serialize};
 use std::str::FromStr;
-use serde::{Serialize, Deserialize};
 
-use nectar_process_lib::{await_message, print_to_terminal, Address, Message, ProcessId, Request, Response};
+use nectar_process_lib::{await_message, println, Address, Message, ProcessId, Request, Response};
 
 wit_bindgen::generate!({
     path: "wit",
@@ -11,83 +11,25 @@ wit_bindgen::generate!({
     },
 });
 
-#[derive(Debug, Serialize, Deserialize)]
-enum ChatRequest {
-    Send { target: String, message: String },
-    History,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-enum ChatResponse {
-    Ack,
-    History { messages: MessageArchive },
-}
-
-type MessageArchive = Vec<(String, String)>;
-
-fn handle_message (
-    our: &Address,
-    message_archive: &mut MessageArchive,
-) -> anyhow::Result<()> {
-    let message = await_message().unwrap();
-
-    match message {
-        Message::Response { .. } => {
-            print_to_terminal(0, &format!("file_transfer: unexpected Response: {:?}", message));
-            panic!("");
-        },
-        Message::Request { ref source, ref body, .. } => {
-            match serde_json::from_slice(body)? {
-                ChatRequest::Send { ref target, ref message } => {
-                    if target == &our.node {
-                        print_to_terminal(0, &format!("file_transfer|{}: {}", source.node, message));
-                        message_archive.push((source.node.clone(), message.clone()));
-                    } else {
-                        let _ = Request::new()
-                            .target(Address {
-                                node: target.clone(),
-                                process: ProcessId::from_str("file_transfer:file_transfer:template.nec")?,
-                            })
-                            .body(body.clone())
-                            .send_and_await_response(5)?
-                            .unwrap();
-                    }
-                    Response::new()
-                        .body(serde_json::to_vec(&ChatResponse::Ack).unwrap())
-                        .send()
-                        .unwrap();
-                },
-                ChatRequest::History => {
-                    Response::new()
-                        .body(serde_json::to_vec(&ChatResponse::History {
-                            messages: message_archive.clone(),
-                        }).unwrap())
-                        .send()
-                        .unwrap();
-                },
-            }
-        },
-    }
+fn handle_message(our: &Address) -> anyhow::Result<()> {
+    let message = await_message()?;
+    println!("file_transfer: got message!: {:?}", message);
     Ok(())
 }
 
 struct Component;
 impl Guest for Component {
     fn init(our: String) {
-        print_to_terminal(0, "file_transfer: begin");
+        println!("file_transfer: begin");
 
         let our = Address::from_str(&our).unwrap();
-        let mut message_archive: MessageArchive = Vec::new();
 
         loop {
-            match handle_message(&our, &mut message_archive) {
-                Ok(()) => {},
+            match handle_message(&our) {
+                Ok(()) => {}
                 Err(e) => {
-                    print_to_terminal(0, format!(
-                        "file_transfer: error: {:?}",
-                        e,
-                    ).as_str());
-                },
+                    println!("file_transfer: error: {:?}", e);
+                }
             };
         }
     }
