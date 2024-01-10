52c5357efe6d8ece3d0c905f5f71caf31a8bd70e
bitful-pannul <bitful-pancreas@protonmail.com>
2024-01-10 16:51:14 -0300

    add basic request type

--- file_transfer/src/lib.rs
+++ file_transfer/src/lib.rs
@@ -1,7 +1,10 @@
+use nectar_process_lib::{
+    await_message, println,
+    vfs::{self, create_drive, open_dir, Directory},
+    Address, Message, ProcessId, Request, Response,
+};
 use serde::{Deserialize, Serialize};
 use std::str::FromStr;
-
-use nectar_process_lib::{await_message, println, Address, Message, ProcessId, Request, Response};
 
 wit_bindgen::generate!({
     path: "wit",
@@ -11,8 +14,55 @@ wit_bindgen::generate!({
     },
 });
 
-fn handle_message(our: &Address) -> anyhow::Result<()> {
+#[derive(Serialize, Deserialize, Debug)]
+pub enum TransferRequest {
+    ListFiles,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FileInfo {
+    pub name: String,
+    pub size: u64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum TransferResponse {
+    ListFiles(Vec<FileInfo>),
+}
+
+fn handle_transfer_request(
+    our: &Address,
+    source: &Address,
+    body: &Vec<u8>,
+    file_dir: &Directory,
+) -> anyhow::Result<()> {
+    let transfer_request = serde_json::from_slice::<TransferRequest>(body)?;
+
+    match transfer_request {
+        TransferRequest::ListFiles => {
+            println!("hellö");
+        }
+    }
+
+    Ok(())
+}
+
+fn handle_message(our: &Address, file_dir: &Directory) -> anyhow::Result<()> {
     let message = await_message()?;
+
+    match message {
+        Message::Response { .. } => {
+            return Ok(());
+        }
+        Message::Request {
+            ref source,
+            ref body,
+            ..
+        } => {
+            handle_transfer_request(&our, source, body, file_dir)?;
+        }
+    };
+
     println!("file_transfer: got message!: {:?}", message);
     Ok(())
 }
@@ -24,8 +74,11 @@ impl Guest for Component {
 
         let our = Address::from_str(&our).unwrap();
 
+        let drive_path = create_drive(our.package_id(), "files").unwrap();
+        let file_dir = open_dir(&drive_path, false).unwrap();
+
         loop {
-            match handle_message(&our) {
+            match handle_message(&our, &file_dir) {
                 Ok(()) => {}
                 Err(e) => {
                     println!("file_transfer: error: {:?}", e);
