92fc900310055a6d70c72d00d7c68a89b032025a
hosted-fornet <hosted-fornet@protonmail.com>
2024-05-30 19:41:01 -0700

    update to 0.8

--- worker/src/lib.rs
+++ worker/src/lib.rs
@@ -1,40 +1,58 @@
-use serde::{Deserialize, Serialize};
 use std::str::FromStr;
 
+use crate::kinode::process::standard::{ProcessId as WitProcessId};
+use crate::kinode::process::file_transfer::{Address as WitAddress, Request as TransferRequest, WorkerRequest, ProgressRequest, InitializeRequest, ChunkRequest};
 use kinode_process_lib::{
-    await_message, get_blob, println,
+    await_message, call_init, get_blob, println,
     vfs::{open_dir, open_file, Directory, File, SeekFrom},
     Address, Message, ProcessId, Request, Response,
 };
 
 wit_bindgen::generate!({
-    path: "wit",
-    world: "process",
-    exports: {
-        world: Component,
-    },
+    path: "target/wit",
+    world: "file-transfer-template-dot-os-v0",
+    generate_unused_types: true,
+    additional_derives: [serde::Deserialize, serde::Serialize],
 });
 
+impl From<Address> for WitAddress {
+    fn from(address: Address) -> Self {
+        WitAddress {
+            node: address.node,
+            process: address.process.into(),
+        }
+    }
+}
+
+impl From<ProcessId> for WitProcessId {
+    fn from(process: ProcessId) -> Self {
+        WitProcessId {
+            process_name: process.process_name,
+            package_name: process.package_name,
+            publisher_node: process.publisher_node,
+        }
+    }
+}
+impl From<WitAddress> for Address {
+    fn from(address: WitAddress) -> Self {
+        Address {
+            node: address.node,
+            process: address.process.into(),
+        }
+    }
+}
+
+impl From<WitProcessId> for ProcessId {
+    fn from(process: WitProcessId) -> Self {
+        ProcessId {
+            process_name: process.process_name,
+            package_name: process.package_name,
+            publisher_node: process.publisher_node,
+        }
+    }
+}
+
 const CHUNK_SIZE: u64 = 1048576; // 1MB
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum WorkerRequest {
-    Initialize {
-        name: String,
-        target_worker: Option<Address>,
-    },
-    Chunk {
-        name: String,
-        offset: u64,
-        length: u64,
-    },
-    Size(u64),
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-pub enum TransferRequest {
-    Progress { name: String, progress: u64 },
-}
 
 fn handle_message(
     our: &Address,
@@ -46,27 +64,25 @@ fn handle_message(
 
     match message {
         Message::Request {
-            ref source,
             ref body,
             ..
         } => {
-            let request = serde_json::from_slice::<WorkerRequest>(body)?;
-
-            match request {
-                WorkerRequest::Initialize {
+            match serde_json::from_slice(body)? {
+                WorkerRequest::Initialize(InitializeRequest {
                     name,
                     target_worker,
-                } => {
+                }) => {
                     // initialize command from main process,
                     // sets up worker, matches on if it's a sender or receiver.
                     // target_worker = None, we are receiver, else sender.
 
                     // open/create empty file in both cases.
                     let mut active_file =
-                        open_file(&format!("{}/{}", files_dir.path, &name), true)?;
+                        open_file(&format!("{}/{}", files_dir.path, &name), true, None)?;
 
                     match target_worker {
                         Some(target_worker) => {
+                            let target_worker: Address = target_worker.into();
                             // we have a target, chunk the data, and send it.
                             let size = active_file.metadata()?.len;
                             let num_chunks = (size as f64 / CHUNK_SIZE as f64).ceil() as u64;
@@ -87,11 +103,11 @@ fn handle_message(
                                 active_file.read_at(&mut buffer)?;
 
                                 Request::new()
-                                    .body(serde_json::to_vec(&WorkerRequest::Chunk {
+                                    .body(serde_json::to_vec(&WorkerRequest::Chunk(ChunkRequest {
                                         name: name.clone(),
                                         offset,
                                         length,
-                                    })?)
+                                    }))?)
                                     .target(target_worker.clone())
                                     .blob_bytes(buffer)
                                     .send()?;
@@ -109,16 +125,16 @@ fn handle_message(
                     }
                 }
                 // someone sending a chunk to us!
-                WorkerRequest::Chunk {
+                WorkerRequest::Chunk(ChunkRequest {
                     name,
                     offset,
                     length,
-                } => {
+                }) => {
                     let file = match file {
                         Some(file) => file,
                         None => {
                             return Err(anyhow::anyhow!(
-                                "file_transfer: receive error: no file initialized"
+                                "file_transfer worker: receive error: no file initialized"
                             ));
                         }
                     };
@@ -126,7 +142,7 @@ fn handle_message(
                     let bytes = match get_blob() {
                         Some(blob) => blob.bytes,
                         None => {
-                            return Err(anyhow::anyhow!("file_transfer: receive error: no blob"));
+                            return Err(anyhow::anyhow!("file_transfer worker: receive error: no blob"));
                         }
                     };
 
@@ -145,10 +161,10 @@ fn handle_message(
                         };
 
                         Request::new()
-                            .body(serde_json::to_vec(&TransferRequest::Progress {
+                            .body(serde_json::to_vec(&TransferRequest::Progress(ProgressRequest {
                                 name,
                                 progress,
-                            })?)
+                            }))?)
                             .target(&main_app)
                             .send()?;
 
@@ -163,41 +179,37 @@ fn handle_message(
             }
         }
         _ => {
-            println!("file_transfer worker: got something else than request...");
+            println!("worker: got something else than request...");
         }
     }
     Ok(false)
 }
 
-struct Component;
-impl Guest for Component {
-    fn init(our: String) {
-        println!("file_transfer worker: begin");
-        let start = std::time::Instant::now();
+call_init!(init);
+fn init(our: Address) {
+    println!("worker: begin");
+    let start = std::time::Instant::now();
 
-        let our = Address::from_str(&our).unwrap();
+    let drive_path = format!("{}/files", our.package_id());
+    let files_dir = open_dir(&drive_path, false, None).unwrap();
 
-        let drive_path = format!("{}/files", our.package_id());
-        let files_dir = open_dir(&drive_path, false).unwrap();
+    let mut file: Option<File> = None;
+    let mut size: Option<u64> = None;
 
-        let mut file: Option<File> = None;
-        let mut size: Option<u64> = None;
-
-        loop {
-            match handle_message(&our, &mut file, &files_dir, &mut size) {
-                Ok(exit) => {
-                    if exit {
-                        println!(
-                            "file_transfer worker done: exiting, took {:?}",
-                            start.elapsed()
-                        );
-                        break;
-                    }
+    loop {
+        match handle_message(&our, &mut file, &files_dir, &mut size) {
+            Ok(exit) => {
+                if exit {
+                    println!(
+                        "worker: done: exiting, took {:?}",
+                        start.elapsed()
+                    );
+                    break;
                 }
-                Err(e) => {
-                    println!("file_transfer: worker error: {:?}", e);
-                }
-            };
-        }
+            }
+            Err(e) => {
+                println!("worker: error: {:?}", e);
+            }
+        };
     }
 }
